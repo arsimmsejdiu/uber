5173304486b3d054c6b545a94384459d631b584e
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-03-23 01:39:10 +0100

    added some data for the flat list

---
+++ components/NavOptions.jsx
@@ -0,0 +1,27 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "124",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatScreen",
+  },
+];
+
+const NavOptions = () => {
+  return (
+    <View>
+      <Text>NavOptions</Text>
+    </View>
+  );
+};
+
+export default NavOptions;
